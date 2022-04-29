469f0be22ec015c1678d7bd04cf1f8c4690e4e7f
JesusBalam <jesus.balambatun@itsva.edu.mx>
2022-04-29 18:20:12 -0500

    :construction: quit default commit

--- Assets/Scripts/SimpleLoading.cs
+++ Assets/Scripts/SimpleLoading.cs
@@ -8,14 +8,12 @@ public class SimpleLoading : MonoBehaviour {
     private RectTransform rectComponent;
     private Image imageComp;
     public float rotateSpeed = 200f;
-
-    // Use this for initialization
+    
     void Start () {
         rectComponent = GetComponent<RectTransform>();
         imageComp = rectComponent.GetComponent<Image>();
-    }
+    }	
 	
-	// Update is called once per frame
 	void Update () {
         rectComponent.Rotate(0f, 0f, -(rotateSpeed * Time.deltaTime));
     }
